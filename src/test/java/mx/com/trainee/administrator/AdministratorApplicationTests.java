42f0d0657ffe9c5f4ba3473cb5d78d8314b6817a
Traineer <luis.sanmartz@gmail.com>
2024-04-15 23:30:13 -0600

    add source code of eureka-server project

---
+++ src/test/java/mx/com/trainee/administrator/AdministratorApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.trainee.administrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
